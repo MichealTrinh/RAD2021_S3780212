74549c21cbeee006bae8b4546a099548612923ac
s3780212 <s3780212@student.rmit.edu.au>
2021-05-22 15:33:49 +0000

    Created tests for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   has_many :carts, :dependent => :destroy
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]*\z/
-  validates :username, presence: true
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, length: { maximum: 25 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8, maximum: 20 }, allow_nil: true, format: { with: VALID_PASSWORD_REGEX}, confirmation: true
 
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,4 +11,73 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  # tests for username field
+  test "username should be present" do
+    @user.username = " "
+    assert_not @user.valid?
+  end
+
+  test "username should not be longer than 25 characters" do
+    @user.username = "x" * 26
+    assert_not @user.valid?
+  end
+
+  # tests for password field
+  test "password should be present" do
+    @user.password = " "
+    assert_not @user.valid?
+  end
+
+  test "password should not be shorter than 8 characters" do
+    @user.password = "x" * 7
+    assert_not @user.valid?
+  end
+
+  test "password should not be longer than 20 characters" do
+    @user.password = "x" * 21
+    assert_not @user.valid?
+  end
+
+  # tests for email field
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+
+  test "email should not be longer than 100 characters" do
+    @user.email = "x" * 100 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "good emails should pass" do
+    # A list of valid emails
+    valid_emails = %w[joe@apple.com TEST@EXP.ORG joe_s@a-School.net alex.smith@topshop.biz don+z@136.co]
+    valid_emails.each do |email|
+      @user.email = email
+      assert @user.valid?, "#{email.inspect} should be valid"
+    end
+  end
+
+  test "bad emails should be rejected" do
+    invalid_emails = %w[joe@apple,com TEST@@EXP.ORG joe_s@a_School.net alex.smith@top shop.biz don+z@13+6.co]
+    invalid_emails.each do |email|
+      @user.email = email
+      assert_not @user.valid?, "#{email.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    clone = @user.dup
+    clone.email = @user.email.upcase
+    @user.save
+    assert_not clone.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Joe@RAd.oRg.AU"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
