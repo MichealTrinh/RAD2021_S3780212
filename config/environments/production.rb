7d963cdbcc65f6e81e8a4bf7017810f64edf2f37
s3780212 <s3780212@student.rmit.edu.au>
2021-05-22 18:49:10 +0000

    update email test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
       port:                 587,
       domain:               'website.com',
       user_name:            'forgotenemail1123@gmail.com',
-      password:             'forgot321',
+      password:             'qnnyqyyuweimfehz',
       authentication:       'plain',
       enable_starttls_auto: true
   }
