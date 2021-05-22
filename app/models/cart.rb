adec047d7d75936e5c4a8cc10900b15a8e7e4dba
s3780212 <s3780212@student.rmit.edu.au>
2021-05-22 17:05:40 +0000

    Created tests for cart model.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,9 @@ class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :product
   after_create :incrementTimesPurchased
+  validates :color, presence: true
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :size, presence: true
 
   def incrementTimesPurchased
     @product = Product.find_by(:id => Cart.all.last.product_id)
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -11,4 +11,39 @@ class CartTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @cart.valid?
   end
+
+  # tests for volor field
+  test "color should be present" do
+    @cart.color = " "
+    assert_not @cart.valid?
+  end
+
+  # tests for quantity field
+  test "quantity should be present" do
+    @cart.quantity = nil
+    assert_not @cart.valid?
+  end
+
+  test "quantity should be greater than 0" do
+    @cart.quantity = 0
+    assert_not @cart.valid?
+  end
+
+  # tests for size field
+  test "size should be present" do
+    @cart.size = ""
+    assert_not @cart.valid?
+  end
+
+  # tests for product_id field
+  test "product_id should be present" do
+    @cart.product_id = nil
+    assert_not @cart.valid?
+  end
+
+  # tests for user_id field
+  test "user_id should be present" do
+    @cart.user_id = nil
+    assert_not @cart.valid?
+  end
 end
