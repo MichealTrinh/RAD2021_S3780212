dd2dd44706d1ae68dba105b5dc66bdee21fd3c95
s3780212 <s3780212@student.rmit.edu.au>
2021-05-22 18:06:43 +0000

    Email testing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def test_email(id)
-    @email = Subscription.find(id).email
+  def test_email
+    @email = params[:email]
     @url  = 'http://example.com/login'
     mail(to: @email, subject: 'Test email from RAD Assignment')
   end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,11 @@
 class Subscription < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   before_save { self.email = email.downcase }
+  after_create :emailSubscribed
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
+
+  def emailSubscribed
+    @email = Subscription.last
+    UserMailer.with(email: @email).test_email.deliver_now
+  end
 end
